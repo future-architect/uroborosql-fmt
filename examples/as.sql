e3ead5e150a5a5680b06f7c5094d5ada8f29bb5a
Kawabuchi Kota(川渕 皓太)A <kawabuchiI062@future.co.jp>
2022-08-31 13:49:55 +0900

    コメントが来ても処理できるように変更。(ANDの前だけ未対応)

--- examples/as.sql
+++ examples/as.sql
@@ -3,12 +3,14 @@
 /* test */
 select 
 --test
-a, 
-b 
+a, --test
+b  --test
 /* multi */
 --test
 from tab1 /*ttesttestesttesetetssss */
  AS t1 --test
  , tab2 AS t2, tabtabtabtabtab AS long  --test
-where  
-        1  = 2 + 1
+where  --test
+        1  = 2 + 1 --test
+AND ttesttestesttesetetssss = t1 --test tests
+--test
