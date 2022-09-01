c6638f23742daf0e20059bf836c1c30aff63b1f6
Toshiya Saito <t.saito.dz@future.co.jp>
2022-09-01 18:42:04 +0900

    コメントを追加、変数名を変更

--- tests/test_all.rs
+++ tests/test_all.rs
@@ -8,20 +8,29 @@ use std::{
 
 #[test]
 fn test_all_files() {
+    // testの対象を格納するディレクトリ
     let src_dir = path::PathBuf::from("./testfiles/src/");
 
-    let files = src_dir.read_dir().unwrap();
-    for dir_entry in files {
-        let src_path = dir_entry.unwrap().path();
+    // src_dirに含まれるすべてのファイル、ディレクトリ
+    let entries = src_dir.read_dir().unwrap();
+    for entry in entries {
+        let src_path = entry.unwrap().path();
 
+        // ファイルかどうかをチェック
         if src_path.is_file() {
+            // file名
             let file_name = src_path.file_name().unwrap().to_str().unwrap();
-            let src = read_to_string(&src_path).unwrap();
+            // fileの内容
+            let content = read_to_string(&src_path).unwrap();
 
-            let result = uroborosql_fmt::format_sql(src.as_str());
+            // フォーマット結果
+            let result = uroborosql_fmt::format_sql(content.as_str());
 
+            // 出力先
             let dst_path = String::from("./testfiles/dst/") + file_name;
             let mut dst_file = File::create(dst_path).unwrap();
+
+            // 出力
             dst_file.write_all(result.as_bytes()).unwrap();
         }
     }
