64ec8f63fc7e0758e5de04f097c24dcbcc56a93b
Kawabuchi Kota(川渕 皓太)A <kawabuchiI062@future.co.jp>
2022-09-07 15:07:36 +0900

    コメントの開始位置を修正

---
+++ testfiles/dst/long_bind_param.sql
@@ -0,0 +1,19 @@
+SELECT
+	ID		AS	DATA_ID					-- ID
+,	CODE	AS	DATA_CODE				-- コード
+,	NAME	AS	DATA_NAME				-- 名称
+,	VALUE1	AS	VALUEAAAAAAAAAAAAAAAA
+,	VALUE2								-- 値2
+,	(
+		SELECT
+			VALUE3
+		FROM
+			TABLE2
+	)									-- サブクエリ
+FROM
+	TABLE1
+WHERE
+	ID		=	'DUMMY'									-- IDが'DUMMY'
+AND	VAL1	=	1										-- VAL1が1
+AND	CODE	=	42										-- CODEが42
+OR	VALUE2	=	/*LONGLONGLONGLONG_BIND_PARAMETER*/42
