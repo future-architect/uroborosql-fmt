d3688b22e84688275b1070fc54b42c73bdd202c6
Taishi Naka <62321668+lemonadern@users.noreply.github.com>
2024-09-11 16:43:51 +0900

    Support comments after `SET` keyword (#99)

--- crates/uroborosql-fmt/testfiles/src/update/update.sql
+++ crates/uroborosql-fmt/testfiles/src/update/update.sql
@@ -2,4 +2,13 @@ UPDATE TABLE1 TBL1 -- テーブル1
 SET TBL1.COLUMN2 = 100 -- カラム2
 , TBL1.COLUMN3 = 100 -- カラム3
 WHERE TBL1.COLUMN1	=	10
-    
+;
+
+update
+	T	t
+set -- after set keyword
+-- another comment
+	c	=	c	+	1
+where
+	id	=	1
+;
