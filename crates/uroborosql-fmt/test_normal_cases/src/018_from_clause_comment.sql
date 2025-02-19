6707a5d2dd69dc1d77cf4380259d97ef815a01d5
Taishi Naka <taishi.develop@gmail.com>
2025-02-19 19:03:52 +0900

    コメント付きテストケースを追加

---
+++ crates/uroborosql-fmt/test_normal_cases/src/018_from_clause_comment.sql
@@ -0,0 +1,17 @@
+select a from t -- comment
+;
+
+select a from t.a -- comment
+;
+
+SELECT u.* FROM test_schema
+    .users -- comment
+;
+
+select
+    a
+from longlongtable as l -- so long
+, tab -- no alias
+, table1 as t1 -- normal
+, sososolonglonglong -- so long and no alias
+;
