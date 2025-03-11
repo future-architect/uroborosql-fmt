1201a0fe462533353e4ad169f5605c9ad4ecb181
Taishi Naka <taishi.develop@gmail.com>
2025-03-11 19:58:15 +0900

    feature: support where_clause

--- crates/uroborosql-fmt/src/new_visitor/clause/where_clause.rs
+++ crates/uroborosql-fmt/src/new_visitor/clause/where_clause.rs
@@ -1,9 +1,10 @@
+use postgresql_cst_parser::syntax_kind::SyntaxKind;
 use tree_sitter::TreeCursor;
 
 use crate::{
     cst::*,
     error::UroboroSQLFmtError,
-    new_visitor::{create_clause, ensure_kind, Visitor},
+    new_visitor::{create_clause, ensure_kind, pg_create_clause, pg_ensure_kind, Visitor},
 };
 
 impl Visitor {
@@ -31,6 +32,33 @@ impl Visitor {
         // cursorをwhere_clauseに戻す
         cursor.goto_parent();
         ensure_kind(cursor, "where_clause", src)?;
+
+        Ok(clause)
+    }
+
+    pub(crate) fn pg_visit_where_clause(
+        &mut self,
+        cursor: &mut postgresql_cst_parser::tree_sitter::TreeCursor,
+        src: &str,
+    ) -> Result<Clause, UroboroSQLFmtError> {
+        cursor.goto_first_child();
+
+        // cursor -> WHERE
+        let mut clause = pg_create_clause(cursor, SyntaxKind::WHERE)?;
+        cursor.goto_next_sibling();
+        self.pg_consume_comments_in_clause(cursor, &mut clause)?;
+
+        // cursor -> a_expr
+        let expr = self.visit_a_expr(cursor, src)?;
+
+        // 結果として得られた式をBodyに変換する
+        let body = Body::from(expr);
+
+        clause.set_body(body);
+
+        // cursorをwhere_clauseに戻す
+        cursor.goto_parent();
+        pg_ensure_kind(cursor, SyntaxKind::where_clause, src)?;
 
         Ok(clause)
     }
