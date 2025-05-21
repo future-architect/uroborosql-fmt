e7480706893364d0bf330f55568c3bb20035987a
Taishi Naka <taishi.develop@gmail.com>
2025-05-21 18:54:19 +0900

    feat: add leading_comma testcase for select all, distinct, distinct on

--- crates/uroborosql-fmt/test_normal_cases/dst/080_2way_extra_leading_comma_select.sql
+++ crates/uroborosql-fmt/test_normal_cases/dst/080_2way_extra_leading_comma_select.sql
@@ -4,3 +4,28 @@ select
 from
 	t
 ;
+select
+	all
+,	c1	as	c1
+,	c2	as	c2
+from
+	t
+;
+select
+	distinct
+,	c1	as	c1
+,	c2	as	c2
+from
+	t
+;
+select
+	distinct on
+		(
+			c1
+		,	c2
+		)
+,	c1	as	c1
+,	c2	as	c2
+from
+	t
+;
--- crates/uroborosql-fmt/test_normal_cases/src/080_2way_extra_leading_comma_select.sql
+++ crates/uroborosql-fmt/test_normal_cases/src/080_2way_extra_leading_comma_select.sql
@@ -3,3 +3,25 @@ select
 ,	c2
 from t
 ;
+select all
+,	c1	as	c1
+,	c2	as	c2
+from
+	t
+;
+select distinct
+,	c1	as	c1
+,	c2	as	c2
+from
+	t
+;
+select distinct on
+	(
+		c1
+	,	c2
+	)
+,	c1	as	c1
+,	c2	as	c2
+from
+	t
+;
