30d5a8c113876b7430fa8fe85975721dfa81c69b
Taishi Naka <taishi.develop@gmail.com>
2025-05-28 20:26:49 +0900

    feat: add 2way mode testcases

---
+++ crates/uroborosql-fmt/test_normal_cases/src/086_2way_mode_if.sql
@@ -0,0 +1,14 @@
+select
+  *
+from
+  employee  emp
+where
+
+emp.birth_date  between  /*birth_date_from*/'1990-01-01'  and  /*birth_date_to*/'1999-12-31'
+
+/*IF SF.isNotEmpty(birth_date_from) and SF.isNotEmpty(birth_date_to)*/
+limit 
+all
+OFFSET 5
+/*END*/
+;
