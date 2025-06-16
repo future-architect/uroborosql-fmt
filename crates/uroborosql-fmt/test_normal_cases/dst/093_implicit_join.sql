88dbf591f47ff69b6522f120195b355baa587f3d
Taishi Naka <taishi.develop@gmail.com>
2025-06-16 20:32:23 +0900

    rename testcase

--- crates/uroborosql-fmt/test_normal_cases/dst/093_implicit_join.sql
+++ crates/uroborosql-fmt/test_normal_cases/dst/093_implicit_join.sql
@@ -5,7 +5,7 @@ from
 -- comment after keyword 1
 -- comment after keyword 2
 	table1
-,	table2	-- t2_comment
+,	table___2	-- t2_comment
 ,	table3	t3
 ,	table4	t4	-- t4_comment
 ,	(
@@ -23,4 +23,14 @@ from
 			inner_table1	-- inner_table1_comment
 		,	inner_table2	-- inner_table2_comment
 	)	t7
+;
+select /* _SQL_ID_ */
+	*
+from
+	t1
+inner join
+	t2
+on
+	t1.num	=	t2.numnumnum	-- test
+and	t1.num	=	t2.num			-- trailing
 ;
