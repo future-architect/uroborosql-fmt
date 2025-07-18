1f4d99041a7ea63f4a75ee59c0962098a3188bf4
lemonadern <taishi.develop@gmail.com>
2025-07-18 15:37:59 +0900

    set panic fook

--- crates/uroborosql-fmt-wasm/src/lib.rs
+++ crates/uroborosql-fmt-wasm/src/lib.rs
@@ -1,9 +1,10 @@
-mod utils;
-
 use uroborosql_fmt::format_sql;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn format_sql_for_wasm(src: &str, config_json_str: &str) -> Result<String, String> {
+    #[cfg(feature = "console_error_panic_hook")]
+    console_error_panic_hook::set_once();
+
     format_sql(src, Some(config_json_str), None).map_err(|e| e.to_string())
 }
