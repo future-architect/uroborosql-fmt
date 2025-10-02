7f4b0a2043b036a83c10c1ca4795de478495ee5e
kawabuchi4280 <k.kawabuchi.z8@future.co.jp>
2025-10-02 18:39:46 +0900

    ~add add testcase

---
+++ crates/uroborosql-fmt/test_normal_cases/dst/106_bind_param_columnref.sql
@@ -0,0 +1,9 @@
+select /* _SQL_ID_ */
+	*
+from
+	tbl
+where
+	tbl./*$targetColumnName1*/col1											is	null
+and	schema1.tbl./*$targetColumnName2*/col2
+and	/*$targetColumnName1*/col1	+	tbl./*$targetColumnName2*/col2	=	10	is	not	null
+;
