1531731345563e850e6bb1c8a513bec30edea288
Taishi Naka <62321668+lemonadern@users.noreply.github.com>
2025-11-13 14:11:49 +0900

    feat(linter rule): no-not-in, no-wildcard-projection (#217)
    Co-authored-by: Taishi Naka <lemonadern@users.noreply.github.com>

--- crates/uroborosql-lint/src/rules.rs
+++ crates/uroborosql-lint/src/rules.rs
@@ -1,7 +1,11 @@
 mod no_distinct;
+mod no_not_in;
 mod no_union_distinct;
+mod no_wildcard_projection;
 mod too_large_in_list;
 
 pub use no_distinct::NoDistinct;
+pub use no_not_in::NoNotIn;
 pub use no_union_distinct::NoUnionDistinct;
+pub use no_wildcard_projection::NoWildcardProjection;
 pub use too_large_in_list::TooLargeInList;
